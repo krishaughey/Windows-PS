f99f83c60d1a643cf40ff4d749f5c7efecfcd454
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-05-30 22:15:58 -0700

    day filter on icalcs

--- USER-Prompt-ProfileCleanup.ps1
+++ USER-Prompt-ProfileCleanup.ps1
@@ -1,11 +1,13 @@
 ## Local User Profile Cleanup
-##### takes ownership, looks for user folders older than x days, and deletes them real good
+##### takes ownership on folders older than x days and removes them
+##### author: Kristopher F. Haughey
 
-$Computer = Read-Host -Prompt "Enter the computer name --->"
-$Days = Read-Host -Prompt "Delete folder(s) older than how many days? --->"
+$Computer = Read-Host -Prompt "Enter the computer name --->" -ForegroundColor Green
+$Days = Read-Host -Prompt "Delete folder(s) older than how many days? --->" -ForegroundColor Green
 
-icacls \\$Computer\c$\Users\*.* /T /grant administrators:F
+Write-Host "Granting Administrators Full Rights on \Users..."
+icacls \\$Computer\c$\Users\*.* /T /grant administrators:F | where {$_.LastWriteTime -le $(get-date).Adddays(-$days)}
 
+Write-Host "Removing User directories older than $Days..." -ForegroundColor Yellow
 Get-ChildItem -Path "\\$Computer\c$\Users\" -Directory | where {$_.LastWriteTime -le $(get-date).Adddays(-$days)} | Remove-Item -recurse
-
-#invoke-command -scriptblock {dir -Include build -Depth 1} | where {$_.LastWriteTime -le $(get-date).Adddays(-$days)} | Remove-Item -recurse -Force -whatif
+Write-Host "Process completed on $Computer" -ForegroundColor Green
