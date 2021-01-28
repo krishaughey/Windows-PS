05cda67343917ae60e72fbcfa882dc2f37918770
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2021-01-27 20:13:20 -0600

    features

--- TEAMS-ClearUserCache.ps1
+++
@@ -1,5 +0,0 @@
-$Teams = Get-Process | Where-Object {$_.ProcessName -eq "Teams"}
-$Teams | Stop-Process
-$TeamsAppData = Get-ChildItem -Recurse "$env:userprofile\AppData\Roaming\Microsoft\Teams"
-$TeamsAppData | Remove-Item
-Start-Process -FilePath "$env:userprofile\AppData\Local\Microsoft\Teams\current\Teams.exe"
