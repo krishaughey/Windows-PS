158be2d85d0d75baeb07a15433cdb1ff1fc82cce
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2022-08-29 11:30:58 -0700

    changed to run under invoke-command

--- COMPUTER-List-DiskFreeSpace.ps1
+++ COMPUTER-List-DiskFreeSpace.ps1
@@ -1,15 +1,14 @@
-## Get Free Disk Space on List
+ ## Get Free Disk Space on List
 ##### Get Free & Used Disk Space on a list of servers - report to CSV
 ##### author: Kristopher F. Haughey
 Import-Module ActiveDirectory
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
 $ServerList = Get-Content c:\Temp\ServerList.txt
 
-$ErrorActionPreference = 'silentlycontinue'
 Write-Host "Gathering info. Depending on your searbase, this might take some time... Grab a coffee?" -ForegroundColor Green
 $Array = @()
 foreach ($Server in $ServerList){
-$colItems = Get-WmiObject Win32_LogicalDisk -ComputerName $Server.DNSHostName
+$colItems = Invoke-Command -Computername $Server -ScriptBlock {Get-WmiObject Win32_LogicalDisk} 
   foreach ($Drive in $colItems){
     $Array += New-Object PsObject -Property ([ordered]@{
       'ServerName' = $Drive.PSComputerName
