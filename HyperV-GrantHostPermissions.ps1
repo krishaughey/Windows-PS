72c8e47dbeb23a8a7adc86236c8e81d9ac330762
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2022-11-02 10:16:03 -0700

    sanitize

--- HyperV-GrantHostPermissions.ps1
+++ HyperV-GrantHostPermissions.ps1
@@ -2,9 +2,9 @@ $Rule1 = $VH11+':(OI)(CI)(GR,WD,AD)'
 $Rule2 = $VH12+':(OI)(CI)(GR,WD,AD)'
 $Rule3 = $VH13+':(OI)(CI)(GR,WD,AD)'
 
-$VH11 = "card\VH11$"
-$VH12 = "card\VH12$"
-$VH13 = "card\VH13$"
+$VH11 = "<DOMAIN>VH11$"
+$VH12 = "<DOMAIN>VH12$"
+$VH13 = "<DOMAIN>VH13$"
 
 $Volumes = Get-ChildItem "C:\ClusterStorage\" | Select-Object FullName
 Foreach($Volume in $Volumes){
--- SOFTWARE-SoftwareDiscovery.ps1
+++ SOFTWARE-SoftwareDiscovery.ps1
@@ -1,7 +1,7 @@
 # REQUIRES THE GETREMOTEPROGRAM FUNCTION
 #$ErrorActionPreference = 'silentlycontinue'
 Import-Module "c:\Temp\Scripts\Modules\SOFTWARE-FUNCTION-GetRemoteProgram.ps1"
-$colItems = get-adcomputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' -SearchBase "OU=Servers,DC=CARD,DC=COM"| Select-Object Name
+$colItems = get-adcomputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' -SearchBase "OU=Servers,DC=<Domain>,DC=COM"| Select-Object Name
 $Array = @()
 foreach ($S in $colItems) {
 	Get-RemoteProgram -computername $S.Name -IncludeProgram 'Sophos Endpoint Agent'
