d4838210e103d9b170c0ea3ad2883c013938723e
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-04-26 09:20:07 -0700

    WMI local hardware report

--- COMPUTER-Local-GetHardwareInfo.ps1
+++ COMPUTER-Local-GetHardwareInfo.ps1
@@ -1,38 +1,37 @@
- ## System Hardware Information
+## System Hardware Information
 ##### Get Hardware Info from WMI of Local Computer - report to CSV
 ##### author: Kristopher F. Haughey
 
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
+$hostname = hostname
 
-#!!DRAFT!!
+Write-Host "Gathering hardware information from $hostname..." -ForegroundColor Cyan
 $Array = @()
-$BIOS = Get-CimInstance Win32_BIOS
-$CPU = Get-CimInstance Win32_Processor
-$Display = Get-CimInstance CIM_Display
-$OS = Get-CimInstance Win32_OperatingSystem
-$Storage = Get-CimInstance Win32_LogicalDisk
-$SYS = Get-CimInstance Win32_ComputerSystem
+$BIOS = Get-CimInstance Win32_BIOS | Select-Object Caption,SerialNumber
+$Chassis = Get-CimInstance CIM_Chassis | Select-Object Description
+$Display = Get-CimInstance CIM_Display | Select-Object Name,MonitorManufacturer
 $IPAddress = Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object {$_.ipaddress -notlike $null}
-$PhysicalMemory = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | % { [Math]::Round(($_.sum / 1GB), 2) }
+$Memory = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | % { [Math]::Round(($_.sum / 1GB), 2) }
+$OS = Get-CimInstance Win32_OperatingSystem | Select-Object CSName,Locale,Caption,Version
+$Storage = Get-CimInstance Win32_LogicalDisk | Measure-Object -Property Size -Sum | % { [Math]::Round(($_.sum / 1GB)) } 
+$SYS = Get-CimInstance Win32_ComputerSystem | Select-Object Manufacturer,Model
 
-  foreach ($Drive in $colItems){
-    $Array += New-Object PsObject -Property ([ordered]@{
-      'Name' = $OS.Name
-      'Manufacturer' = $SYS.Manufacturer
-      'Model' = $SYS.Model
-      'Asset Type' = Chassis Type
-      'Serial Number' = $BIOS.SerialNumber
-      'Location' = 
-      'IP Address' = $IPAddress.IPaddress[0]
-      'Operating System' = $CompInfo.WindowsProductName
-      'Operating System Version' = $SYS.OsVersion
-      'Firmware' = $CompInfo.BiosCaption
-      'Storage Capacity' = [Math]::Round($Storage.Size / 1GB)
-      'RAM' = $PhysicalMemory
-      'Monitor Manufacturer' =  $Display.Name[0,1] 
-      'Monitor Model' =  $Display.MonitorManufacturer[0,1]
-      })
-    }
+  $Array += New-Object PsObject -Property ([ordered]@{
+    'Name' = $OS.CSName
+    'Manufacturer' = $SYS.Manufacturer
+    'Model' = $SYS.Model
+    'Asset Type' = $Chassis.Description
+    'Serial Number' = $BIOS.SerialNumber
+    'Location' = $OS.Locale
+    'IP Address' = $IPAddress.IPaddress[0]
+    'Operating System' = $OS.Caption
+    'Operating System Version' = $OS.Version
+    'Firmware' = $BIOS.Caption
+    'Storage Capacity' = $Storage
+    'RAM' = $Memory
+    'Monitor Manufacturer' =  $Display.Name[0]
+    'Monitor Model' =  $Display.MonitorManufacturer[0]
+    })
 
-$Array | export-csv C:\%USERPROFILE%\Downloads\SystemReport_$timestamp.csv -NoTypeInformation
-Write-Host "export = C:\%USERPROFILE%\Downloads\SystemReport_$timestamp.csv" -ForegroundColor Cyan
+$Array | Export-Csv $ENV:USERPROFILE\Downloads\$hostname-SystemReport_$timestamp.csv -NoTypeInformation
+Write-Host "Report Complete! $ENV:USERPROFILE\Downloads\$hostname-SystemReport_$timestamp.csv" -ForegroundColor Cyan
