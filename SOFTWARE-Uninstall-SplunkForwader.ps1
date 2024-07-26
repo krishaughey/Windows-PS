6b127118376a57a3eeeee7e78b18664919b7a954
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2024-07-26 15:25:28 -0700

    uninstall splunk

---
+++ SOFTWARE-Uninstall-SplunkForwader.ps1
@@ -0,0 +1,19 @@
+#Uninstall SplunkForwarder
+## Stop Splunk services and uninstall the SplunkForwarder application
+#### https://github.com/krishaughey
+
+### Check for Splunk Service and Process
+$ServiceCheck = Get-Service SplunkForwarder
+if ($ServiceCheck -ne $Null) {
+
+    ### Stop and Delete Service
+    Stop-Service SplunkForwarder
+    sc.exe delete SplunkForwarder
+
+    ### Uninstall Application
+    $Splunk = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "UniversalForwarder"}
+    $Splunk.Uninstall()
+}
+else {
+    Write-Output "Splunk Service Not Installed"
+}
